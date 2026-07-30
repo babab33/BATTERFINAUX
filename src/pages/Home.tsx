6a90590ad07b548536b447b17026fe6a7f0d5ae7
babab33 <basschen555@gmail.com>
2026-07-29 22:02:53 -0400

    Fix Call button and add Google phone data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -430,7 +430,7 @@ const Home = () => {
               Get Your Free Quote
             </Link>
             <a
-              href="https://www.google.com/search?sca_esv=e6a856e083a4051b&rlz=1C5CHFA_enCA1165CA1167&sxsrf=AE3TifP1CznZIaK2U9YtKbqEmVEZ6V4fzQ:1753633128793&si=AMgyJEtREmoPL4P1I5IDCfuA8gybfVI2d5Uj7QMwYCZHKDZ-E0dX6aSKVVGe1vAssDtCht5Ax0zPBlsk_qt9Cemz0GBd6c9tuIdwpF4kpo0GRWnPG0yfrj0wn3VdZZKFuBW-hkIOp9ek87JWbMHfVNNCeg22DmLEr3ruLtu7XXRzShqZg5cifpCzx5k8vXeyl0aHXML3LQAM&q=Batteiger+Lawn+Service.+Snow+Removal+%26+Weed+Control+Avis&sa=X&ved=2ahUKEwiq7tS9uN2OAxXqv4kEHQXnIFEQ0bkNegQIHxAE&biw=1470&bih=831&dpr=2#lrd=0x5320a3a83ec6660d:0xa31a8a7e77811f54,3,,,,"
+              href="tel:+17018189370"
               className="border-2 border-white text-white px-8 py-4 rounded-lg font-semibold text-lg hover:bg-white hover:text-red-600 transition-all duration-300 inline-flex items-center justify-center"
             >
               Call (701) 818-9370
